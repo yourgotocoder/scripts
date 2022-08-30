25e635284d1e5ac13050432f93485a22769ae1f9
yourgotocoder <yourgotocoder@gmail.com>
2022-08-30 15:14:09 +0530

    Changes to Desktop Setup

--- DESKTOP_SETUP/Desktop_AutoSetup_Sudu.sh
+++ DESKTOP_SETUP/Desktop_AutoSetup_Sudu.sh
@@ -1,16 +1,17 @@
 sudo apt-get update
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget https://downloads.mongodb.com/compass/mongodb-compass_1.15.1_amd64.deb
+sudo apt-get install curl openjdk-8-jdk-headless openjdk-8-jre-headless build-essential
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt-get install curl
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source ~/.bashrc
 nvm install v16.17.0
 sudo snap install --classic code
 sudo snap install spotify
-wget https://downloads.mongodb.com/compass/mongodb-compass_1.15.1_amd64.deb
+
 sudo dpkg -i mongodb-compass_1.15.1_amd64.deb
 sudo snap install mysql-workbench-community
-sudo apt-get install openjdk-8-jdk-headless openjdk-8-jre-headless build-essential
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
- sudo add-apt-repository universe
+sudo add-apt-repository universe
 sudo apt install gnome-tweaks
---
+++ DESKTOP_SETUP/README.md
@@ -0,0 +1,4 @@
+To get started with my desktop setup copy this folder anywhere in your fresh Ubuntu 22.04LTS installation. 
+This might work for older or newer distros too but I haven't tried it for them so I can only conclude that it will work for v22.04 for now.
+Once you have copied the folder in your desktop. You will have to turn the **Desktop_AutoSetup_Sudu.sh** file into an executable file using the **sudo chmod +x Desktop_AutoSetup_Sudu.sh** command. 
+The rest should be taken care of by the script. 
