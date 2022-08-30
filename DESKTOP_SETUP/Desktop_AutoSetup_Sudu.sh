e43981d0146bfa10799044edf578a390bf690ce8
yourgotocoder <yourgotocoder@gmail.com>
2022-08-30 15:43:35 +0530

    A bit more information about theme setup

--- DESKTOP_SETUP/Desktop_AutoSetup_Sudu.sh
+++ DESKTOP_SETUP/Desktop_AutoSetup_Sudu.sh
@@ -1,7 +1,7 @@
-sudo apt-get update
+sudo apt-get update -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-wget https://downloads.mongodb.com/compass/mongodb-compass_1.15.1_amd64.deb
-sudo apt-get install curl openjdk-8-jdk-headless openjdk-8-jre-headless build-essential
+wget https://downloads.mongodb.com/compass/mongodb-compass_1.30.1_amd64.deb
+sudo apt-get install -y curl openjdk-8-jdk-headless openjdk-8-jre-headless build-essential gtk2-engines-murrine gtk2-engines-pixbuf plank
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source ~/.bashrc
@@ -9,9 +9,18 @@ nvm install v16.17.0
 sudo snap install --classic code
 sudo snap install spotify
 
-sudo dpkg -i mongodb-compass_1.15.1_amd64.deb
+sudo dpkg -i mongodb-compass_1.30.1_amd64.deb
 sudo snap install mysql-workbench-community
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 sudo add-apt-repository universe
 sudo apt install gnome-tweaks
+tar xf Mojave-Dark.tar.xz
+mkdir ~/.themes
+mv Mojave-Dark ~/.themes
+tar xf Mojave-CT-Classic.tar.xz
+mkdir ~/.icons
+mv Mojave-CT-Classic ~/.icons
+unzip -qq macOS\ Cursor\ Set.zip
+mv macOS\ Cursor\ Set ~/.icons
+sudo apt-get remove gnome-shell-extension-ubuntu-dock aisleriot gnome-sudoku gnome-mines gnome-mahjongg
--- DESKTOP_SETUP/README.md
+++ DESKTOP_SETUP/README.md
@@ -6,8 +6,18 @@
 To get started with my desktop setup copy this folder anywhere in your fresh Ubuntu 22.04LTS installation. 
 This might work for older or newer distros too but I haven't tried it for them so I can only conclude that it will work for v22.04 for now.
 </p>
+
 <p>
 Once you have copied the folder in your desktop. You will have to turn the <b>Desktop_AutoSetup_Sudu.sh</b> file into an executable file using the <b>sudo chmod +x Desktop_AutoSetup_Sudu.sh</b> command. 
-The rest should be taken care of by the script.
-
+The installation should be taken care of by the script.
 </p>
+
+### THEME SETUP
+
+<p>Search for gnome-tweaks in your application window that can be accessed using the Windows key. In the gnome-tweak interface select Appearance then select</p>
+
+<ul>
+    <li><b>MacOS Cursor Set</b> for <b>Cursor</b></li>
+    <li><b>Mojave-CT-Classic</b> for <b>Icons</b></li>
+    <li><b>Mojave-Dark</b> for <b>Legacy Applications</b></li>
+</ul>
