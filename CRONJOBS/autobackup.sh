7eda948e83f0a4e3c04e4b125685adaafc5a7556
yourgotoocder <yourgotocoder@gmail.com>
2022-08-30 09:46:33 +0530

    Fixed the a minor bug in my autobackup script

--- CRONJOBS/autobackup.sh
+++ CRONJOBS/autobackup.sh
@@ -1,11 +1,11 @@
                         #!/bin/bash
 DATE=$(date +%d-%m-%Y)
-
+BACKUP_DIR='/media/mountpoint_a'
 # mount extra hdd to the mounting point that you created
-sudo mount /dev/sdb1 /media/mountpoint_a/
+sudo mount /dev/sdb1 $BACKUP_DIR/
 
 # take backup with dates
-tar -zcvpf /media/mountpoint_a/cse-$DATE.tar.gz /home/cse/
+tar -zcvpf $BACKUP_DIR/cse-$DATE.tar.gz /home/cse/
 
 # Delete files older than 10 days #
 find $BACKUP_DIR/* -mtime +10 -exec rm {} \;
