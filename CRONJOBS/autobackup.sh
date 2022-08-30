c9d196d44bee6ae7d702899ede4227cd8c233230
yourgotoocder <yourgotocoder@gmail.com>
2022-08-30 09:41:23 +0530

    Updated autobackup script

--- CRONJOBS/autobackup.sh
+++ CRONJOBS/autobackup.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
+                        #!/bin/bash
 DATE=$(date +%d-%m-%Y)
-BACKUP_DIR=”/backup”
 
-# take backup in separate name, use below format #
-tar -zcvpf $BACKUP_DIR/yeahhub-$DATE.tar.gz /home/cse/
+# mount extra hdd to the mounting point that you created
+sudo mount /dev/sdb1 /media/mountpoint_a/
+
+# take backup with dates
+tar -zcvpf /media/mountpoint_a/cse-$DATE.tar.gz /home/cse/
 
 # Delete files older than 10 days #
 find $BACKUP_DIR/* -mtime +10 -exec rm {} \;
+
+# unmount after backup is complete, -l flag waits for all processes to be 
+# done on the mounted drive
+sudo umount -l /dev/sdb1
 
