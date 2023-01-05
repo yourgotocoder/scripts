3b0ad45bfac6a7ca27c381e27671c256d81b8b62
yourgotocoder <yourgotocoder@gmail.com>
2023-01-05 15:10:11 +0530

    Mysql updates

--- LAB_SERVER/MYSQL_SETUP/mysql-statement-generator.js
+++ LAB_SERVER/MYSQL_SETUP/mysql-statement-generator.js
@@ -6,7 +6,6 @@ const data = parser.parseXls2Json(__dirname + "/4thSemPasscode.xlsx")[0];
 for (let student of data) {
   fs.appendFileSync(
     __dirname + "/student.sql",
-    `CREATE DATABASE db_${student.REGNO};
-    GRANT ALL PRIVILEGES ON db_${student.REGNO}.* to 'cse_${student.REGNO}'@'localhost' with grant option;`
+    `CREATE DATABASE db_${student.REGNO};\nCREATE USER 'cse_${student.REGNO}'@'localhost' IDENTIFIED BY '${student.PASSCODE}';\nGRANT ALL PRIVILEGES ON db_${student.REGNO}.* to 'cse_${student.REGNO}'@'localhost' with grant option;\n`
   );
 }
