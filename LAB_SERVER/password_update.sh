9ee571adc779013ddc81459d5556cf05baf658c9
yourgotocoder <yourgotocoder@gmail.com>
2023-02-27 10:31:13 +0530

    Password update modified for my use case

--- LAB_SERVER/password_update.sh
+++ LAB_SERVER/password_update.sh
@@ -5,11 +5,11 @@ filename="users.csv"
 while read line; do
   user=$(echo $line | cut -d ',' -f1)
   password=$(echo $line | cut -d ',' -f2)
-  echo "Updating password for user $user"
-  echo "$password" | passwd --stdin "$user"
+  echo "Updating password for user cse_$user"
+  echo "$password" | passwd --stdin "cse_$user"
   if [ $? -eq 0 ]; then
-    echo "Password updated successfully for user $user"
+    echo "Password updated successfully for user cse_$user with $password"
   else
-    echo "Error updating password for user $user"
+    echo "Error updating password for user cse_$user"
   fi
 done < "$filename"
